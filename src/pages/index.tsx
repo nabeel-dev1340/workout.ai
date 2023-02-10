a57938fc35526b91e16ff1098b6b70f36b0dbf01
nabeel-dev1340 <nabeelsharafat@gmail.com>
2023-02-10 17:02:17 +0500

    api integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import NavBar from "components/NavBar";
 
 export default function Home() {
+  //TODO: Add default values when UI is implemented
+  const [time, setTime] = useState(30);
+  const [muscle, setMuscle] = useState("chest");
+  const [equipment, setEquipment] = useState("dumbbell");
+  const [location, setLocation] = useState("gym");
+
+  const [workoutData, setWorkoutData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!time || !muscle || !equipment || !location) {
+        return;
+      }
+
+      const response = await fetch(
+        `https://wild-gray-goshawk-wrap.cyclic.app/?time=${time}&muscle=${muscle}&location=${location}&equipment=${equipment}`
+      );
+      const data = await response.json();
+      setWorkoutData(data);
+    };
+
+    fetchData();
+  }, [time, muscle, equipment, location]);
   return (
     <>
       <Head>
@@ -11,7 +35,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <NavBar/>
+        <NavBar />
+        {workoutData && <div>{JSON.stringify(workoutData)}</div>}
       </main>
     </>
   );
