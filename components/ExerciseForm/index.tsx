2bb3586af9be4299604313bd08986188974b4525
hr-hassan-raza <hassan@gradience.io>
2023-02-11 17:17:23 +0500

    minor styling

--- components/ExerciseForm/index.tsx
+++ components/ExerciseForm/index.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { FormControl, FormLabel, Select, Checkbox, Stack } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Select,
+  Checkbox,
+  Stack,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
 
-const muscles = [
-  "Chest",
-  "Back",
-  "Shoulders",
-  "Arms",
-  "Legs",
-  "Abs"
-];
+const muscles: string[] = ["Chest", "Back", "Shoulders", "Arms", "Legs", "Abs"];
 
 const equipment: string[] = [
   "Dumbbells",
@@ -16,84 +17,95 @@ const equipment: string[] = [
   "Kettlebells",
   "Resistance Bands",
   "Smith Machine",
-  "Pull Up Bar"
+  "Pull Up Bar",
 ];
 
 const ExerciseForm = () => {
-    const [selectedMuscle, setSelectedMuscle] = useState("");
-    const [gymLocation, setGymLocation] = useState(false);
-    const [homeLocation, setHomeLocation] = useState(false);
-    const [selectedEquipment, setSelectedEquipment] = useState<string[]>([]);
+  const [selectedMuscle, setSelectedMuscle] = useState("");
+  const [gymLocation, setGymLocation] = useState(false);
+  const [homeLocation, setHomeLocation] = useState(false);
+  const [selectedEquipment, setSelectedEquipment] = useState<string[]>([]);
 
-    const handleMuscleSelection = (event: any) => {
-        setSelectedMuscle(event.target.value);
-    };
+  const handleMuscleSelection = (event: any) => {
+    setSelectedMuscle(event.target.value);
+  };
 
-    const handleLocationSelection = (location: string) => {
-        if (location === "gym") {
-            setGymLocation(!gymLocation);
-        } else {
-            setHomeLocation(!homeLocation);
-        }
-    };
+  const handleLocationSelection = (location: string) => {
+    if (location === "gym") {
+      setGymLocation(!gymLocation);
+    } else {
+      setHomeLocation(!homeLocation);
+    }
+  };
 
-    const handleEquipmentSelection = (equipment: string) => {
-        setSelectedEquipment(prevEquipment => {
-            if (prevEquipment.includes(equipment)) {
-                return prevEquipment.filter(e => e !== equipment);
-            } else {
-                return [...prevEquipment, equipment];
-            }
-        });
-    };
+  const handleEquipmentSelection = (equipment: string) => {
+    setSelectedEquipment((prevEquipment) => {
+      if (prevEquipment.includes(equipment)) {
+        return prevEquipment.filter((e) => e !== equipment);
+      } else {
+        return [...prevEquipment, equipment];
+      }
+    });
+  };
 
-    return (
-        <form>
-            <FormControl>
-                <FormLabel htmlFor="muscle-select">Muscles</FormLabel>
-                <Select
-                    id="muscle-select"
-                    value={selectedMuscle}
-                    onChange={handleMuscleSelection}
-                >
-                    <option value="">Select a muscle group</option>
-                    {muscles.map(muscle => (
-                        <option key={muscle} value={muscle}>
-                            {muscle}
-                        </option>
-                    ))}
-                </Select>
-            </FormControl>
+  return (
+    <Flex justifyContent={"center"} alignItems={"center"}>
+      <form>
+        <FormControl>
+          <FormLabel htmlFor="muscle-select">
+            {" "}
+            <Text as="strong"> Muscles</Text>
+          </FormLabel>
 
-            <Stack mt={4}>
-                <FormLabel>Location</FormLabel>
-                <Checkbox
-                    isChecked={gymLocation}
-                    onChange={() => handleLocationSelection("gym")}
-                >
-                    Gym
-                </Checkbox>
-                <Checkbox
-                    isChecked={homeLocation}
-                    onChange={() => handleLocationSelection("home")}
-                >
-                    Home
-                </Checkbox>
-            </Stack>
+          <Select
+            id="muscle-select"
+            value={selectedMuscle}
+            onChange={handleMuscleSelection}
+            width="300px"
+          >
+            <option value="">Select a muscle group</option>
+            {muscles.map((muscle) => (
+              <option key={muscle} value={muscle}>
+                {muscle}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
 
-            <Stack mt={4}>
-                <FormLabel>Equipment</FormLabel>
-                {equipment.map(e => (
-                    <Checkbox
-                        key={e}
-                        isChecked={selectedEquipment.includes(e)}
-                        onChange={(e) => handleEquipmentSelection(e as unknown as string)}
-                    >
-                        {e}
-                    </Checkbox>
-                ))}
-            </Stack>
-        </form>
-    )
-}
-export default ExerciseForm
+        <Stack mt={4}>
+          <FormLabel>
+            <Text as="strong"> Location </Text>
+          </FormLabel>
+          <Checkbox
+            isChecked={gymLocation}
+            onChange={() => handleLocationSelection("gym")}
+          >
+            Gym
+          </Checkbox>
+          <Checkbox
+            isChecked={homeLocation}
+            onChange={() => handleLocationSelection("home")}
+          >
+            Home
+          </Checkbox>
+        </Stack>
+
+        <Stack mt={4}>
+          <FormLabel>
+            <Text as="strong"> Equipment</Text>
+          </FormLabel>
+          {equipment.map((e) => (
+            <Checkbox
+              key={e}
+              isChecked={selectedEquipment.includes(e)}
+              onChange={(e) => handleEquipmentSelection(e as unknown as string)}
+            >
+              {e}
+            </Checkbox>
+          ))}
+        </Stack>
+      </form>
+    </Flex>
+  );
+};
+export default ExerciseForm;
